6a73d18795589a31846be6fbebe24346a74ea2ea
vigneshvicky123456 <vignesh.j2002@gmail.com>
2023-12-05 23:54:02 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    name: "AlmaBetter",
+    subscribedChannel: "WEB",
+  },
+  {
+    name: "John Doe",
+    subscribedChannel: "freeCodeCamp.org",
+  },
+  {
+    name: "vignesh",
+    subscribedChannel: "W3School",
+  },
+  {
+    name: "Deepak",
+    subscribedChannel: "Udemy",
+  },
+];
+
+module.exports = data;
